07b288593a0a1f48098cf26248f20edc3a15c5eb
snitzelbread <julian.duss@renuo.ch>
2026-08-11 15:53:37 +0200

    move method?

--- activerecord/lib/active_record/enum.rb
+++ activerecord/lib/active_record/enum.rb
@@ -218,16 +218,6 @@ module ActiveRecord
       _enum(name, values, **options)
     end
 
-    def define_pairs(values, name)
-      if values.respond_to?(:each_pair)
-        values.each_pair
-      elsif values.is_a?(Array) && type_for_attribute(name).subtype.is_a?(ActiveModel::Type::String)
-        values.to_h { |value| [value.to_sym, value.to_s] }
-      else
-        values.each_with_index
-      end
-    end
-
     private
       def _enum(name, values, prefix: nil, suffix: nil, scopes: true, instance_methods: true, validate: false, **options)
         values = assert_valid_enum_definition_values(values)
@@ -382,6 +372,16 @@ module ActiveRecord
         end
 
         values
+      end
+
+      def define_pairs(values, name)
+        if values.respond_to?(:each_pair)
+          values.each_pair
+        elsif values.is_a?(Array) && type_for_attribute(name).subtype.is_a?(ActiveModel::Type::String)
+          values.to_h { |value| [value.to_sym, value.to_s] }
+        else
+          values.each_with_index
+        end
       end
 
       def assert_valid_enum_options(options)
